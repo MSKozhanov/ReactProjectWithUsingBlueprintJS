053ae0127c7b8cb1414a6121109407fc170631d0
MSKozhanov <kozhanov312@gmail.com>
2022-07-22 09:35:28 +0300

    is working

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,15 @@ import { MenuComponent } from './MenuComponent/index'
 
 function App(props:any) { 
     const [isOpen, setIsOpen] = useState<boolean>(false);
+    const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
     return (
         <div>
-            <div>
+            <div >
                 <h1> Click to open dialog with table </h1>
-                
-                <MenuComponent/>
                 <Button onClick={() => { setIsOpen(true) }}>Open Dialog</Button>
-                <FirstComponent isOpen={isOpen} onCloseDialog={(val:boolean)=>{setIsOpen(val)}} />
+                <FirstComponent isOpen={isOpen} onCloseDialog={(val: boolean) => { setIsOpen(val) }} />
+                <Button>ABC</Button>
+                <MenuComponent/>
                 
                 
             </div>
--- src/MenuComponent/index.tsx
+++ src/MenuComponent/index.tsx
@@ -1,40 +1,90 @@
 import React, { useState } from "react";
-import { Button, Menu, MenuItem } from "@blueprintjs/core";
+import { Button, Card, Classes, Elevation, Intent, Menu, MenuItem, PopoverInteractionKind, Position } from "@blueprintjs/core";
 import { Popover2 } from "@blueprintjs/popover2";
+import '@blueprintjs/popover2/lib/css/blueprint-popover2.css';
 
 
 
 export const MenuComponent: React.FC = () => {
-
-  const array = [1,2,3];
-  
+  const [array, setArray] = useState<number[]>([]);
+  const [currentCount, setCurrentCount] = useState<number>(0);
   const randElem = () => {
     let val = 100;
     let randomValue = Math.floor(Math.random() * val)
     console.log('random', randomValue);
     return randomValue;
   }
-  const addElement = (randomValue: number, array: number[] ) => {
-    
-    console.log('arr', array);
-    array.push(randomValue)
-    console.log('arrNew', array);
-    return array;
-  }
-  
-  const menu = (
-    <Menu >
-      <MenuItem icon="download" text="Download" onClick={() => addElement(randElem(),array)}  /> 
-      <MenuItem icon="download" text="Download2" />
-    </Menu>
-  );
- 
+  const addElement = () => {
+    let a: number[] = array;
+    setCurrentCount(currentCount + 1);
+    a.push(randElem());
+    setArray(a);
+  };
+  // const menu = (
+  //   <Menu  style={{ width: "200px" }}>
+  //       <MenuItem icon="add" text="Add element" onClick={addElement} />{" "}
+        
+  //       <MenuItem icon="menu-open" text="Elements"
+          
+  //         children={array.map((element, index) => (
+  //           <MenuItem icon="small-plus" key={index} text={element} />
+  //         ))}
+  //       />
+  //     </Menu>
+  // );
+
+  console.log("render");
   return (
-
-    <Popover2 content={menu}>
-      <Button intent="primary" text="OpenMenu" />
-    </Popover2>
-
+    <Card>
+      <Popover2 position={Position.RIGHT_TOP} className={Classes.DARK}   >
+      
+      <Menu  style={{ width: "200px" }}>
+        <MenuItem icon="add" text="Add element" onClick={addElement} />
+        
+        <MenuItem icon="menu-open" text="Elements" children={array.map((element, index) => (
+            <MenuItem icon="small-plus" key={index} text={element} />
+          ))}
+          
+        />
+      </Menu>
+      </Popover2>
+      </Card>
   );
+
+  
+
+  // const addElement = () => { 
+  //   let array:number[] = arrayList
+  //   array.push(randElem()) 
+  //   console.log('arrNew', array);
+  //   setArrayList(array)
+  // }
+  // const getMenuItem = () => { console.log("arrL",arrayList)
+  //   let newMenuItem = arrayList.map((element, index) => { console.log(111); return <MenuItem icon="small-plus" key={index} text={element} /> });
+  //   return newMenuItem
+  // } 
+  // const menu = (
+  //   <Menu >
+  //     <MenuItem icon="plus" text="Add element" onClick={addElement} /> {/**onClick={() => addElement()} */}
+  //     <MenuItem  icon="menu-open" text="Elements">   
+  //       {/* {getMenuItem()} */}
+  //       { arrayList.map((element, index) =>  <MenuItem icon="small-plus" key={index} text={index} /> )}
+  //     </MenuItem>
+  //   </Menu> 
+  //// );
+  
+
+//   return (
+    // <Popover2 content={menu} position={Position.RIGHT_TOP} className={Classes.DARK} >
+    //   <Button intent="primary" text="OpenMenu" />
+    // </Popover2>
+// <Menu >
+//       <MenuItem icon="plus" text="Add element" onClick={addElement} /> {/**onClick={() => addElement()} */}
+//       <MenuItem icon="menu-open" text="Elements">   
+//         {/* {getMenuItem()} */}
+//         { arrayList.map((element, index) =>  <MenuItem icon="small-plus" key={index} text={index} /> )}
+//       </MenuItem>
+//     </Menu> 
+//   );
 };
 
